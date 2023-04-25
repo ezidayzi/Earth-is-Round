9db116ca3cb3f776784f8836285931f2ea883e3e
L-j-h-c <ckrgkswnsgh@naver.com>
2023-04-25 21:56:25 +0900

    [Feat] #26 - ERButton 추가

--- Projects/DesignSystem/Sources/Components/ERButton.swift
+++ Projects/DesignSystem/Sources/Components/ERButton.swift
@@ -6,4 +6,42 @@
 //  Copyright © 2023 com.earthIsRound. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+public struct ERButton: ButtonStyle {
+    var labelColor = ERColor.Black50
+    var backgroundColor = ERColor.Black90
+    
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(DesignSystemIosFontFamily.AritaDotumOTF.semiBold.font(size: 18).toSwiftUI)
+            .foregroundColor(labelColor)
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 17)
+            .background(backgroundColor)
+            .clipShape(Capsule())
+            .dynamicCornerRadius(backgroundColor)
+    }
+    
+    public init(
+        labelColor: Color = ERColor.Black50,
+        backgroundColor: Color = ERColor.Black90
+    ) {
+        self.labelColor = labelColor
+        self.backgroundColor = backgroundColor
+    }
+}
+
+extension View {
+    public func erButton(
+        labelColor: Color = ERColor.Black50,
+        backgroundColor: Color = ERColor.Black90
+    ) -> some View {
+        buttonStyle(
+            ERButton(
+                labelColor: labelColor,
+                backgroundColor: backgroundColor
+            )
+        )
+    }
+}
--- Projects/DesignSystem/Sources/Components/ERTextField.swift
+++ Projects/DesignSystem/Sources/Components/ERTextField.swift
@@ -10,19 +10,19 @@ import SwiftUI
 
 public struct ERTextField: View {
     @Binding private var text: String
-    @Binding private var isValid: Bool
+    private var isValid: Bool
     private var maxLength: Int
     private var placeholder: String
     @FocusState private var isFocused: Bool
     
     public init(
         text: Binding<String>,
-        isValid: Binding<Bool>,
+        isValid: Bool,
         maxLength: Int,
         placeholder: String
     ) {
         self._text = text
-        self._isValid = isValid
+        self.isValid = isValid
         self.maxLength = maxLength
         self.placeholder = placeholder
     }
@@ -31,7 +31,7 @@ public struct ERTextField: View {
         ZStack(alignment: .leading) {
             if(text.isEmpty && !isFocused) {
                 Text(placeholder)
-                    .foregroundColor(.gray)
+                    .foregroundColor(ERColor.Black50)
             }
             TextField("", text: $text, prompt: Text(placeholder))
                 .focused($isFocused)
@@ -40,6 +40,8 @@ public struct ERTextField: View {
                 }
         }
         .padding(EdgeInsets(top: 10, leading: 25, bottom: 10, trailing: 25))
-        .dynamicCornerRadius(isValid: isValid, ERColor.Alert, ERColor.Black50)
+        .background(ERColor.Black90)
+        .clipShape(Capsule())
+        .dynamicCornerRadius(isValid: isValid, ERColor.Alert, ERColor.Black90)
     }
 }
