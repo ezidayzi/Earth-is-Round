a3bcb307006a3ebf4e4362dc16915f281bc09a31
Yoonseo Kim <yo26on@soongsil.ac.kr>
2023-09-05 23:55:25 +0900

    [Feat] #56 메인화면 디자인 변경사항 반영

--- Projects/iOSApp/Features/MainFeature/Demo/Sources/MainFeatureDemoApp.swift
+++ Projects/iOSApp/Features/MainFeature/Demo/Sources/MainFeatureDemoApp.swift
@@ -10,7 +10,7 @@ struct MainFeatureDemoApp: App {
         WindowGroup {
             MainView(
                 store: Store(
-                    initialState: MainFeature.State(),
+                    initialState: MainFeature.State(currentWeekDay: 18, pastSteps: [13, 15, 400]),
                     reducer: MainFeature()
                 )
             )
@@ -22,7 +22,7 @@ struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView(
             store: Store(
-                initialState: MainFeature.State(),
+                initialState: MainFeature.State(currentWeekDay: 18, pastSteps: [13, 15, 400]),
                 reducer: MainFeature()
             )
         )
